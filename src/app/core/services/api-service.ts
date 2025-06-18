a7305450bd77ceeac7e88ae5b59167cdc8472880
rougemeister <rougemeister1@gmail.com>
2025-06-18 13:03:33 +0000

    Extend api service to support pagination

--- src/app/core/services/api-service.ts
+++ src/app/core/services/api-service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { API_URL, ACCESS_KEY } from '../constants/contants';
 import { Post, PostWithImage } from '../model/model';
@@ -25,6 +25,27 @@ export class ApiService {
       )
     );
   }
+
+  getPaginatedPosts(page: number, limit: number): Observable<Post[]> {
+    const params = new HttpParams()
+      .set('_page', page.toString())
+      .set('_limit', limit.toString());
+
+    return this.http.get<Post[]>(API_URL, { params });
+  }
+
+  getPaginatedPostsWithImages(page: number, limit: number): Observable<PostWithImage[]> {
+    return this.getPaginatedPosts(page, limit).pipe(
+      map((posts) =>
+        posts.map((post) => ({
+          ...post,
+          imageUrl: `https://picsum.photos/seed/${post.id}/600/400`,
+        }))
+      )
+    );
+  }
+
+  
 }
 
 
